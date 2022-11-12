0782d798f0166bffcf8c83bd62fdd266a51534cc
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-11-12 21:16:42 +0100

    Add 512x icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        "name": "Klods",
-        "short_name": "Klods",
-        "start_url": "/",
-        "display": "standalone",
-        "icons": [
+        name: "Klods",
+        short_name: "Klods",
+        start_url: "/",
+        display: "standalone",
+        icons: [
           {
-            "src": "/icons/klods-64x.png",
-            "sizes": "64x64",
-            "type": "image/png",
-            "purpose": "any"
+            src: "/icons/klods-64x.png",
+            sizes: "64x64",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            "src": "/icons/klods-128x.png",
-            "sizes": "128x128",
-            "type": "image/png",
-            "purpose": "any"
+            src: "/icons/klods-128x.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            "src": "/icons/klods-192x.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
+            src: "/icons/klods-192x.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            "src": "/icons/klods-256x.png",
-            "sizes": "256x256",
-            "type": "image/png",
-            "purpose": "any"
-          }
-        ]
-      }
-    })
-  ]
-})
+            src: "/icons/klods-256x.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/klods-512x.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
+});
