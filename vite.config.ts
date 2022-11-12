0bd4fe1491df3fa7a7c19dd804670836f68082f8
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-11-12 19:54:15 +0100

    Add icons and manifest

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,40 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA()
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "Klods",
+        "short_name": "Klods",
+        "start_url": "/",
+        "display": "standalone",
+        "icons": [
+          {
+            "src": "/icons/klods-64x.png",
+            "sizes": "64x64",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/icons/klods-128x.png",
+            "sizes": "128x128",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/icons/klods-192x.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/icons/klods-256x.png",
+            "sizes": "256x256",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ]
+      }
+    })
   ]
 })
