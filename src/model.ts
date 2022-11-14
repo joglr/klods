801c386062b9d23f58a9e58936f0f5d31ab3aa69
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-11-14 17:36:47 +0100

    Add picking up pieces functionality and refactor

--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,20 @@
+export type IBoard = (ISquare | null)[]
+
 export interface ISquare {
   hue: number
+}
+
+export type IPiece = number[][]
+
+export interface IState {
+  highscore: number
+  board: IBoard
+  userPieces: IPiece[]
+  selectedPiece: null | IPieceInfo
+  score: 0
+}
+
+interface IPieceInfo {
+  index: number
+  location: [number, number]
 }
--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,10 @@
+import { boardSize } from "./constants"
+import type { IBoard, IPiece } from "./model"
+
+export const createEmptyBoard = () =>
+  Array.from({ length: Math.pow(boardSize, 2) }).map((_, i) => (null))
+
+export const createRainbowBoard = () =>
+  Array.from({ length: Math.pow(boardSize, 2) }).map((_, i) => ({ hue: (i * 60) % 360 }))
+
 export const choose = <T>(values: T[]) => values[Math.floor(Math.random() * values.length)]
