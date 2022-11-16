1b494d805fe518914ea2e221bf17a0fc5a6abfce
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-11-16 18:30:09 +0100

    Add failing test

--- src/__tests__/checkIfPieceFits.test.ts
+++ src/__tests__/checkIfPieceFits.test.ts
@@ -3,6 +3,17 @@ import { checkIfPieceFits, createEmptyBoard } from "../util"
 
 describe("checkIfPieceFits", () => {
   it("succeeds with placing 3x3 wedge piece in bottom right corner", () => {
+    /*
+      | 0  1  2  3  4  5  6  7
+    0 |
+    1 |
+    2 |
+    3 |
+    4 |
+    5 |                      1
+    6 |                      1
+    7 |                1  1  X
+    */
     const succeesState1: IState = {
       highscore: 0,
       board: createEmptyBoard(),
@@ -19,5 +30,35 @@ describe("checkIfPieceFits", () => {
       succeesSquareLocation1
     )
     expect(result).toBe(true)
+  })
+
+  it("fails with wedge piece on top of occupied pieces", () => {
+    /*
+      | 0  1  2  3  4  5  6  7
+    0 |
+    1 |
+    2 |
+    3 |
+    4 |
+    5 |
+    6 |                   X  X
+    7 |                   X  X
+    */
+    const failureState: IState = {
+      highscore: 0,
+      board: [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,{"hue":200},{"hue":200},null,null,null,null,null,null,{"hue":200},{"hue":200}],
+      userPieces: [[[1,1],[1,0]],[[1,1,1,1],[1,0,0,0]],[[1,1],[0,1],[0,1],[0,1]]],
+      selectedPiece: {"index":0,"location":[0,0]},
+      score: 0,
+    }
+
+    const failureSquareLocation: [number, number] = [6,6]
+    const result = checkIfPieceFits(
+      failureState.board,
+      failureState.userPieces[failureState.selectedPiece!.index]!,
+      failureState.selectedPiece!.location,
+      failureSquareLocation
+    )
+    expect(result).toBe(false)
   })
 })
