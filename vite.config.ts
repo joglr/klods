fe6f88ad304fd3f381bbd2256af9887c56b00d7b
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-11-17 22:01:31 +0100

    Use minimal UI

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         name: "Klods",
         short_name: "Klods",
         start_url: "/",
-        display: "standalone",
+        display: "minimal-ui",
         theme_color: "#00cc00",
         icons: [
           {
