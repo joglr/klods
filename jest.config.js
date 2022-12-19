7a767fac6cbff3a680f6a97103e3d33b7e9edd07
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2022-12-19 11:59:42 +0000

    Export jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testEnvironment": "node",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "moduleNameMapper": {
+    "^.+\\.(css|less)$": "identity-obj-proxy"
+  }
+}
